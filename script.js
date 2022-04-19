fd824008685e4cfc21530eff98ced79d4932c7f9
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-04-18 22:17:02 -0400

    Updating

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ let movieDescription = $('.modal').modal();
 let trailerID;
 //add click event listener ffor main search function
 
-<<<<<<< HEAD
-=======
 searchBtn.on("click", function () {
   //assign variable, targeting input value in DOM
   let title = $("input").val();
@@ -57,61 +55,7 @@ searchBtn.on("click", function () {
 
 });
 
-function renderTrailerByImdbId(imdbId){
-
-searchBtn.on("click", function() {
-    //assign variable, targeting input value in DOM
-    let title = $("input").val();
-
->>>>>>> 585d455bf964aa075759244be01f3912b23b2143
-    //assign variable to URL, concatenating title and APIkey
-    let requestUrl = "http://www.omdbapi.com/?s=" + title + "&apikey=528cb2fd";
-    //perform an API Call to request and return JSON data from OMDB
-    fetch(requestUrl)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            moviesCard.html("");
-            data.Search.forEach(function(movie) {
-                let container = $("<div>", { class: 'col s3 m3' });
-
-                let card = $("<div>", { class: 'card' });
-
-                let imgTag = $("<div>", { class: 'card-image' });
-
-                let poster = $("<img>", { src: movie.Poster, height: 532 });
-
-                let cardTitle = $("<span>", { class: 'card-title' });
-                cardTitle.text(movie.Title);
-                imgTag.append(poster);
-                card.append(imgTag);
-                imgTag.append(cardTitle);
-
-                let trailer = $("<div>", { class: 'card-action' });
-
-                let trailerBtn = $("<button>", { class: 'btn trailerBtn' });
-                trailerBtn.data('data', movie.imdbID)
-                trailerBtn.text("Trailer");
-                trailer.append(trailerBtn);
-
-                let descriptionBtn = $("<button>", { class: 'btn descriptionBtn' });
-                descriptionBtn.text("Description");
-                descriptionBtn.data('data', movie.imdbID)
-
-                trailer.append(descriptionBtn);
-                card.append(trailer);
-                container.append(card);
-                moviesCard.append(container);
-            })
-        });
-});
-
 function renderTrailerByImdbId(imdbId) {
-<<<<<<< HEAD
-=======
-
->>>>>>> 585d455bf964aa075759244be01f3912b23b2143
     console.log('click')
     let moviesUrl = "https://imdb-api.com/en/API/Trailer/k_g52895d2/" + imdbId
     fetch(moviesUrl)
@@ -190,6 +134,7 @@ function renderDescriptionbyImdb(imdbId) {
 //event listener for play button to retrieve trailer data on click
 $(document).on('click', '.trailerBtn', function(e) {
     const imdbId = $(e.target).data('data')
+    console.log("trailerBtn")
     renderTrailerByImdbId(imdbId)
 });
 
@@ -216,3 +161,4 @@ function getWatchlist() {
     localStorage.getItem("selectTitle")
     return JSON.parse(selectTitle);
 };
+}
